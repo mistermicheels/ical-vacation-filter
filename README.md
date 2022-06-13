77874535e9a5d0bfcda5da9148b0e608291a2fe4
mistermicheels <22038058+mistermicheels@users.noreply.github.com>
2022-06-13 23:34:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ical-vacation-filter
+
+Take an Outlook iCal feed and keep only the out-of-office events.
+
+Filtered feed URL: `https://ical-vacation-filter.herokuapp.com/filter?source=%source_feed_URL%`.
+
+It is recommended to encode the source feed URL using EncodeURIComponent.
+
+Note: If you struggle to get your favourite calendar software to subscribe to the resulting feed URL, try putting the URL behind a URL shortener.
+
+## Suggestions for improvement
+
+-   Validate that source is provided
+-   Validate that source actually returns an iCal feed and not something else
+-   Add timeout and potentially max source feed size
+-   Provide nice error messages in case of timeout, invalid source URL, ...
+-   Add frontend that turns source feed URL into copyable filtered feed URL
+-   Process source feed as a stream so we can filter huge iCal feeds with minimal memory overhead
+-   Add automated tests
---
+++ index.js
@@ -0,0 +1,36 @@
+const axios = require("axios").default;
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+/**
+ * @param {(req: express.Request, res: express.Response) => any} callback
+ * @returns {express.RequestHandler}
+ */
+const asyncWrap = (callback) => (req, res, next) => callback(req, res).catch(next);
+
+app.get(
+    "/filter",
+    asyncWrap(async (req, res) => {
+        const sourceUrl = req.query.source;
+        const sourceResponse = await axios.get(sourceUrl);
+
+        /** @type {string} */
+        const sourceData = sourceResponse.data;
+
+        const fullEventRegex = /^BEGIN:VEVENT.*?^END:VEVENT\r?\n/gms;
+        const isEventOutOfOfficeRegex = /^X-MICROSOFT-CDO-BUSYSTATUS:OOF$/gms;
+
+        const filteredData = sourceData.replace(fullEventRegex, (event) =>
+            isEventOutOfOfficeRegex.test(event) ? event : ""
+        );
+
+        res.header(sourceResponse.headers);
+        res.send(filteredData);
+    })
+);
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+});
