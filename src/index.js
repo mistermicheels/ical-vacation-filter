8aed898123e30f6af4f24d53af9360ec7574d2bb
mistermicheels <22038058+mistermicheels@users.noreply.github.com>
2022-06-19 13:05:46 +0200

    refactor: extract data retrieval and filtering

--- src/index.js
+++ src/index.js
@@ -10,23 +10,35 @@ const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
  */
 const asyncWrap = (callback) => (req, res, next) => callback(req, res).catch(next);
 
+/**
+ * @param {string} sourceUrl
+ * @returns {Promise<{ sourceData: string, sourceHeaders: Record<string, any> }>}
+ */
+const getSourceDataAndHeaders = async (sourceUrl) => {
+    const sourceResponse = await axios.get(sourceUrl);
+    return { sourceData: sourceResponse.data, sourceHeaders: sourceResponse.headers };
+};
+
+/**
+ * @param {string} sourceData
+ * @returns {string}
+ */
+const filterEvents = (sourceData) => {
+    const fullEventRegex = /^BEGIN:VEVENT.*?^END:VEVENT\r?\n/gms;
+    const isEventOutOfOfficeRegex = /^X-MICROSOFT-CDO-BUSYSTATUS:OOF$/gms;
+
+    return sourceData.replace(fullEventRegex, (event) =>
+        isEventOutOfOfficeRegex.test(event) ? event : ""
+    );
+};
+
 app.get(
     "/filter",
     asyncWrap(async (req, res) => {
         const sourceUrl = req.query.source;
-        const sourceResponse = await axios.get(sourceUrl);
-
-        /** @type {string} */
-        const sourceData = sourceResponse.data;
-
-        const fullEventRegex = /^BEGIN:VEVENT.*?^END:VEVENT\r?\n/gms;
-        const isEventOutOfOfficeRegex = /^X-MICROSOFT-CDO-BUSYSTATUS:OOF$/gms;
-
-        const filteredData = sourceData.replace(fullEventRegex, (event) =>
-            isEventOutOfOfficeRegex.test(event) ? event : ""
-        );
-
-        res.header(sourceResponse.headers);
+        const { sourceData, sourceHeaders } = await getSourceDataAndHeaders(sourceUrl);
+        const filteredData = filterEvents(sourceData);
+        res.header(sourceHeaders);
         res.send(filteredData);
     })
 );
