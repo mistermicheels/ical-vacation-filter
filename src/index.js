dff4824cb9da8aaece5d0e3839f086d06b229263
mistermicheels <22038058+mistermicheels@users.noreply.github.com>
2022-07-24 11:12:12 +0200

    harcode NODE_ENV to "production"
    ensure local app behavior matches production without needing cross-env

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// hardcode NODE_ENV to "production" so behavior always matches production behavior
+process.env.NODE_ENV = "production";
+
 const { app } = require("./app");
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
